55dbd9e492a9e29040584ec394c9824714b2843f
vlad <momka12@ukr.net>
2017-03-21 18:38:27 +0200

    db

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,13 @@
+CREATE TABLE usersJAVA.dbo.users (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  lastname VARCHAR(25) NOT NULL,
+  city VARCHAR(25),
+  email VARCHAR(35)
+);
+CREATE UNIQUE INDEX users_id_uindex ON users (id);
+CREATE UNIQUE INDEX users_email_uindex ON users (email);
+
+INSERT INTO users VALUES ('petrik', 'ivanov', 'dnepr', 'petr@mail.com');
+INSERT INTO users VALUES ('maria', 'forma', 'odessa', 'maria@mail.com');
+INSERT INTO users VALUES ('romka', 'fich', 'dnepr', 'romka@mail.com');
